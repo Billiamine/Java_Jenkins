9982d01f00c4b9f584515264b78ccbc62c25a32d
Billiamine <79008725+Billiamine@users.noreply.github.com>
2022-10-26 17:10:40 +0200

    Update affichage_build.java

--- affichage_build.java
+++ affichage_build.java
@@ -1,7 +1,6 @@
 
-  class Formation devops Goldentechnoly {
-  public static void main(String[] args) {
-  System.out.println("Formation devops Goldentechnoly");
-  }
+class Formation devops Goldentechnoly{  
+    public static void main(String args[]){  
+     System.out.println("Formation devops Goldentechnoly");  
+    }  
 }
-
